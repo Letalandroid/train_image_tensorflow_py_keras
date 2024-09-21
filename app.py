642868678eb169c75be680bdf201a448693578aa
UCV-PIURA <ucvpiura@ucv.edu.pe>
2024-09-21 15:29:18 -0500

    Finished

--- app.py
+++ app.py
@@ -12,16 +12,14 @@ model = load_model("keras_Model.h5", compile=False)
 class_names = open("labels.txt", "r").readlines()
 
 # Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 # Replace this with the path to your image
-image = Image.open("<IMAGE_PATH>").convert("RGB")
+image = Image.open("1.jpg").convert("RGB")
 
 # resizing the image to be at least 224x224 and then cropping from the center
 size = (224, 224)
-image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+image = ImageOps.fit(image, size, Image.LANCZOS)  # Updated here
 
 # turn the image into a numpy array
 image_array = np.asarray(image)
