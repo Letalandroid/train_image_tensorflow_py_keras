24cf55c2659d75b3b5508dbe6944c18cb3020c9a
letalandroid <carlossoncra@gmail.com>
2024-10-14 18:36:54 -0500

    Change locahost to db for remote_context

--- app/config/db.py
+++ app/config/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, MetaData
 
-engine = create_engine("mysql+pymysql://root:root@localhost:5000/danistoredb")
+engine = create_engine("mysql+pymysql://root:root@db:3306/danistoredb") # local: localhost, remote: db
 
 meta = MetaData()
 
