c70724922a0d2e97dd7c4934d345b1d5ac222538
letalandroid <carlossoncra@gmail.com>
2024-10-14 19:13:16 -0500

    Update: + time to app.py & init db first

--- app/config/db.py
+++ app/config/db.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine, MetaData
+from time import sleep
 
+sleep(20)
 engine = create_engine("mysql+pymysql://root:root@db:3306/danistoredb") # local: localhost, remote: db
 
 meta = MetaData()
